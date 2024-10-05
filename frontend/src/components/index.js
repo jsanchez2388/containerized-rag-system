a9bddaab06bb2bc6bfa1ab15b7193c144fd90aa7
Josue Sanchez <30679533+jsanchez2388@users.noreply.github.com>
2024-10-04 18:18:44 -0700

    Added basic file structure. Installed react-router-dom

---
+++ frontend/src/components/index.js
@@ -0,0 +1,6 @@
+// src/components/index.js
+
+export { default as ConversationDisplay } from './ConversationDisplay';
+export { default as QuestionInput } from './QuestionInput';
+export { default as Navbar } from './Navbar';
+export { default as AuthForm } from './AuthForm';
---
+++ frontend/src/routes.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import PageNotFound from './pages/PageNotFound';
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />  {/* Catch-all route for 404 */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRoutes;
